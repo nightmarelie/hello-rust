c6d83df8e72874076853af3541680b1cac5c38ab
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-01-24 17:31:12 -0600

    chore: integer examples

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,29 @@ fn main() {
     // DATA TYPES
     // Scalar types
     // - Integers
+    // Length	Signed	Unsigned
+    // 8-bit	i8	    u8
+    // 16-bit	i16	    u16
+    // 32-bit	i32	    u32
+    // 64-bit	i64	    u64
+    // 128-bit	i128	u128
+    // arch	    isize	usize
+
+    // create all integer types
+    let i8: i8 = 127;
+    let i16: i16 = 32767;
+    let i32: i32 = 2147483647;
+    let i64: i64 = 9223372036854775807;
+    let i128: i128 = 170141183460469231731687303715884105727;
+    let isize: isize = 9223372036854775807;
+
+    let u8: u8 = 255;
+    let u16: u16 = 65535;
+    let u32: u32 = 4294967295;
+    let u64: u64 = 18446744073709551615;
+    let u128: u128 = 340282366920938463463374607431768211455;
+    let usize: usize = 18446744073709551615;
+
     // - Floating-point numbers
     // - Booleans
     // - Characters
