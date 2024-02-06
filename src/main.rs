4e92afaf6524c20952aa33123f27cc20c67596ac
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-06 09:50:10 -0600

    chore: using enum

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 fn main() {
-    let number1 = 15;
-    let number2= 4;
-
-    if number1 > number2 && number1 % number2 == 0 {
-        println!("{} is greater than {} and divisible by {}", number1, number2, number2);
-    } else if number1 > number2 {
-        println!("{} is greater than {}", number1, number2);
-    } else if number1 == number2 {
-        println!("{} is equal to {}", number1, number2);
-    } else {
-        println!("{} is less than {}", number1, number2);
+    enum Direction {
+        Up,
+        Down,
+        Left,
+        Right,
     }
+
+    // we can use the enum to create a variable
+    let player_direction: Direction = Direction::Up;
+
+    // we can also use the enum to create a function
+    fn move_player(d: Direction) {
+        // match is used to compare the value of d
+        match d {
+            Direction::Up => println!("Moving up"),
+            Direction::Down => println!("Moving down"),
+            Direction::Left => println!("Moving left"),
+            Direction::Right => println!("Moving right"),
+        }
+    }
+
+    // call the function with the player_direction variable
+    move_player(player_direction);
 }
