149b8fca2ecea01e84740402ddf1103be9c55af0
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-07 09:50:10 -0600

    chore: use match

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,19 @@ fn main() {
     let player_direction: Direction = Direction::Up;
 
     // we can also use the enum to create a function
-    fn move_player(d: Direction) {
+    fn move_player(d: Direction) -> &'static str {
         // match is used to compare the value of d
         match d {
-            Direction::Up => println!("Moving up"),
-            Direction::Down => println!("Moving down"),
-            Direction::Left => println!("Moving left"),
-            Direction::Right => println!("Moving right"),
+            Direction::Up => "Moving up",
+            Direction::Down => "Moving down",
+            Direction::Left => "Moving left",
+            Direction::Right => "Moving right",
         }
     }
 
     // call the function with the player_direction variable
-    move_player(player_direction);
+    let move_result = move_player(player_direction);
+
+    // print the result
+    println!("{}", move_result);
 }
