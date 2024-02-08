b7456153c53d143f81e55d4b54e5ffb9cbda258f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-08 09:50:10 -0600

    chore: try loop

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 fn main() {
-    enum Direction {
-        Up,
-        Down,
-        Left,
-        Right,
-    }
+    let mut counter = 0;
 
-    // we can use the enum to create a variable
-    let player_direction: Direction = Direction::Up;
-
-    // we can also use the enum to create a function
-    fn move_player(d: Direction) -> &'static str {
-        // match is used to compare the value of d
-        match d {
-            Direction::Up => "Moving up",
-            Direction::Down => "Moving down",
-            Direction::Left => "Moving left",
-            Direction::Right => "Moving right",
+    let result = loop {
+        if counter == 10 {
+            break counter * 2;
         }
-    }
 
-    // call the function with the player_direction variable
-    let move_result = move_player(player_direction);
+        counter += 1;
+    };
 
-    // print the result
-    println!("{}", move_result);
+    println!("The result is {}", result);
 }
