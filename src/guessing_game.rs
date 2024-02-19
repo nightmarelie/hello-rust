1bfd142e7e6fd802c92c50152e8a15c2b4e5a8cf
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-18 19:57:10 -0600

    feat: compare to numbers

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::{thread_rng, Rng};
 
 pub fn run () {
     println!("Guess the number!");
+
+    let secret_number = thread_rng().gen_range(1..102);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -10,4 +16,10 @@ pub fn run () {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    let result = guess.trim().parse::<i32>().expect("Can't parse the guess!").cmp(&secret_number);
+
+    assert_eq!(result, Ordering::Equal);
+
+    println!("The result is: {:?}", result);
 }
