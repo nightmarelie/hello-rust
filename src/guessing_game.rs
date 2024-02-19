5c74a741710fb44ce2833da990958bdc8ec63acc
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-18 20:00:10 -0600

    feat: run guessing_game in the loop

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -9,19 +9,24 @@ pub fn run() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    let guess: i32 = guess.trim().parse().expect("Can't parse the guess to a number!");
+        let guess: i32 = guess.trim().parse().expect("Can't parse the guess to a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
