aa790648c42eeacb8228cc5763dfea9c999ed8be
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-20 22:01:09 -0600

    feat: add ownership and functions examples

--- src/ownership_rules.rs
+++ src/ownership_rules.rs
@@ -15,6 +15,9 @@ pub fn example () {
     // example with variables
     let x = 5;
     let y = x; // x is copied to y, both are valid
+    // all types stored in the stack are copied, because they are stored in the stack
+    // these types are: integers, floats, booleans, characters, tuples (if they contain only types stored in the stack)
+    println!("x = {}, y = {}", x, y);
 
     let s1 = String::from("hello");
     let s2 = s1; // s1 is moved to s2, s1 is no longer valid. It's called a move, not a copy. It's done to avoid double free error
@@ -26,4 +29,35 @@ pub fn example () {
     println!("{}", s1); // s1 is valid
 
     // 2. Ownership and Functions
+    // 2.1 example
+    let s = String::from("hello"); // s comes into scope
+    takes_ownership(s); // s's value moves into the function and so is no longer valid here
+    println!("{}", s); // s is not valid here
+
+    // 2.2 example
+    // gives_ownership will move its return value into s1
+    let s1 = gives_ownership(); // gives_ownership moves its return value into s1
+    println!("{}", s1); // s1 is valid
+
+    // 2.3 example
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2); // s2 is moved into takes_and_gives_back and moves its return value into s3
+    println!("{}", s3); // s3 is valid
+    // println!("{}", s2); // s2 is not valid
+
+    // 3. Return Values and Scope
+    // 3.1 example
 }
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing memory is freed.
+
+fn gives_ownership() -> String { // gives_ownership will move its return value into the function that calls it
+    let some_string = String::from("hello"); // some_string comes into scope
+    some_string // some_string is returned and moves out to the calling function
+}
+
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into scope
+    a_string // a_string is returned and moves out to the calling function
+} // Here, a_string goes out of scope and `drop` is called. The backing memory is freed.
