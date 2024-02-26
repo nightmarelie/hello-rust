fb471570ae4af62dbf2224568aa4e2b5acf47a99
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-25 21:45:30 -0600

    feat: example of combining enums

--- src/enumeration.rs
+++ src/enumeration.rs
@@ -52,11 +52,18 @@ mod tests {
     }
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -64,6 +71,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
