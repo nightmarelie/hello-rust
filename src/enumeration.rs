220e2b13c0e75bbd87623b6d00a0e933f776ce2c
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-25 21:43:15 -0600

    feat: add matcher for coin enum

--- src/enumeration.rs
+++ src/enumeration.rs
@@ -51,3 +51,19 @@ mod tests {
         example(Command::ChangeColor(255, 255, 255));
     }
 }
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
