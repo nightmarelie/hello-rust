451b06e68013b9ad733026f8a88cdef45cbf4719
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-26 21:48:29 -0600

    feat: add absolute and relative path

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
 mod front_of_house {
-    pub mod hosting {
+    mod hosting {
         pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
 
     mod serving {
-        fn take_order() {}
+        fn take_order() {
+            // Relative path
+            super::hosting::add_to_waitlist();
+
+            // Absolute path
+            crate::front_of_house::hosting::add_to_waitlist();
+        }
 
         fn serve_order() {}
 
