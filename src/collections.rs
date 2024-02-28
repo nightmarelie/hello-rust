e4f250629b065cce2222aad5c33b3309648742a9
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-27 20:40:53 -0600

    feat: iterate and update over hash map

--- src/collections.rs
+++ src/collections.rs
@@ -1,6 +1,9 @@
 use unicode_segmentation::UnicodeSegmentation;
+use std::collections::HashMap;
 
 pub fn examples() {
+
+    // Vectors
     // let a = [1, 2, 3, 4, 5];
 
     let mut vec: Vec<i32> = Vec::new(); // type required because of empty vec
@@ -32,6 +35,7 @@ pub fn examples() {
         println!("{}", i);
     }
 
+    // Strings
     // String are UTF-8 encoded
     // let s = String::new();
     // let data = "initial contents";
@@ -39,31 +43,65 @@ pub fn examples() {
     // let s = String::from("hello");
 
     // Updating a string
-    let mut s = String::from("foo");
-    s.push_str("bar");
-    s.push('l');
+    // let mut s = String::from("foo");
+    // s.push_str("bar");
+    // s.push('l');
+    //
+    // // Concatenation
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world!");
+    // let s3 = s1 + &s2; // s1 has been moved here and can no longer be used
+    // let s4 = format!("{}-{}", s2, s3);
+    // // println!("{}", s1); // error here because s1 has been moved
+    // println!("{}", s3);
+    //
+    // // Iterating over strings
+    // for c in "नमस्ते".chars() {
+    //     println!("{}", c);
+    // }
+    //
+    // for b in "नमस्ते".bytes() {
+    //     println!("{}", b);
+    // }
+    //
+    // for g in "नमस्ते".graphemes(true) {
+    //     println!("{}", g);
+    // }
 
-    // Concatenation
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2; // s1 has been moved here and can no longer be used
-    let s4 = format!("{}-{}", s2, s3);
-    // println!("{}", s1); // error here because s1 has been moved
-    println!("{}", s3);
+    // Hash maps
+    let blue = String::from("blue");
+    let yellow = String::from("yellow");
 
-    // Iterating over strings
-    for c in "नमस्ते".chars() {
-        println!("{}", c);
+    let mut scores = HashMap::new();
+    scores.insert(&blue, 10);
+    scores.entry(&blue).or_insert(20); // will not insert because blue is already in the map
+    scores.insert(&yellow, 50);
+
+    println!("{:?}", scores);
+
+    let team_name = String::from("blue");
+    let score = scores.get(&team_name);
+
+    match score {
+        Some(value) => println!("The score for {} is {}", team_name, value),
+        None => println!("There is no score for {}", team_name),
     }
 
-    for b in "नमस्ते".bytes() {
-        println!("{}", b);
+    // iterate over hash map
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
     }
 
-    for g in "नमस्ते".graphemes(true) {
-        println!("{}", g);
+    // ITERATE AND UPDATE
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
     }
 
+    println!("{:?}", map);
 }
 
 
