ae5a5662bbf6e0151af28e08be276e4e21c62401
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-27 20:03:28 -0600

    feat: intro into collections

---
+++ src/collections.rs
@@ -0,0 +1,18 @@
+pub fn examples () {
+    let a = [1, 2, 3, 4, 5];
+
+    let vec: Vec<i32> = Vec::new();
+
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_examples() {
+        examples();
+    }
+}
