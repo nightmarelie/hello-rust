c8e92ced7e0c917a10d3dcdb2c735b849798836f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-28 20:55:40 -0600

    feat: create file using closure

--- src/error.rs
+++ src/error.rs
@@ -27,6 +27,16 @@ pub fn examples() {
             })
         },
     };
+
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Tried to create file but there was a problem: {:?}", error);
+            })
+        } else {
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    });
 }
 
 #[cfg(test)]
