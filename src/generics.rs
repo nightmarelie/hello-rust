3e4baebcba8fda8e4737311d3a871a244d048042
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-29 22:10:15 -0600

    feat: different types

--- src/generics.rs
+++ src/generics.rs
@@ -1,14 +1,16 @@
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 pub fn run() {
     let p = Point { x: 5, y: 10 };
     let a = Point { x: 5.1, y: 10.1 };
+    let n = Point { x: 1, y: 10.1 };
 
     println!("p.x = {}, p.y = {}", p.x, p.y);
-    println!("p.x = {}, p.y = {}", a.x, a.y);
+    println!("a.x = {}, a.y = {}", a.x, a.y);
+    println!("n.x = {}, n.y = {}", n.x, n.y);
 }
 
 #[cfg(test)]
