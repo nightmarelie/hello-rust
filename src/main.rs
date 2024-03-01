feff9efbd4235c48338a085a7e6cf1b744cbda83
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-29 21:25:04 -0600

    feat: add largest

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::error::Error;
+mod largest;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let f = std::fs::File::open("hello.txt")?;
-
-    Ok(())
+fn main() {
+    largest::run();
 }
