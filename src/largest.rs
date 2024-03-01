45eed907dc486b8f1ec9351972d519932c69b0ef
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-02-29 21:32:34 -0600

    feat: add more test cases

--- src/largest.rs
+++ src/largest.rs
@@ -1,4 +1,4 @@
-pub fn run (number_list: Vec<i32>) -> i32 {
+pub fn run<T: PartialOrd + Copy>(number_list: Vec<T>) -> T {
     let mut largest = number_list[0];
 
     for number in number_list {
@@ -15,11 +15,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_run() {
+    fn find_largest_number() {
         let number_list = vec![34, 50, 25, 100, 65];
 
         let result = run(number_list);
 
         assert_eq!(result, 100);
+    }
+
+    #[test]
+    fn find_largest_characters() {
+        let character_list = vec!["a", "b", "c", "d", "e"];
+
+        let result = run(character_list);
+
+        assert_eq!(result, "e");
     }
 }
