7d7534263a6ca38c413b660971460bd705852830
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-01 19:50:10 -0600

    feat: add traits example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-mod largest;
-mod generics;
+mod traits;
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest::run(number_list);
-
-    println!("The largest number is {}", result);
-
-    generics::run();
+    traits::example();
 }
