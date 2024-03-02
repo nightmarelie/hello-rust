1c2d274b459263837dcef49bd09a2fa940303a77
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-01 19:50:11 -0600

    feat: add another feature

--- src/traits.rs
+++ src/traits.rs
@@ -36,7 +36,15 @@ pub fn example () {
         retweet: false,
     };
 
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
+    };
+
     println!("1 new tweet: {}", tweet.summarize());
+    println!("1 new tweet: {}", article.summarize());
 }
 
 #[cfg(test)]
