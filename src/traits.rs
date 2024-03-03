80a314d11461de55dd5ad266f7fd6f72f9c25cbf
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-02 21:01:11 -0600

    feat: traint in return statement

--- src/traits.rs
+++ src/traits.rs
@@ -57,6 +57,15 @@ pub fn some_function<T: Summary + Debug>(item: &T) {
     println!("Breaking news! {:?}", item);
 }
 
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
 // where clause
 pub fn some_function2<T>(item: &T)
 where
