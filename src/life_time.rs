1853b120c0d5d68bddc894fa50a11aa18ff5900b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-03 16:23:26 -0600

    feat(life-time): add doc

--- src/life_time.rs
+++ src/life_time.rs
@@ -20,6 +20,9 @@ pub fn example() {
 }
 
 // using generic life-time annotation
+// &i32       // a reference
+// &'a i32    // a reference with an explicit lifetime
+// &'a mut i32// a mutable reference with an explicit lifetime
 fn longest <'a> (x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
