6e54cdcc6d6096b682a04fb4839009761c5c3174
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-04 21:43:37 -0600

    feat(tests): prepare for test

--- src/tests.rs
+++ src/tests.rs
@@ -1,14 +1,23 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 pub fn test() {
-    // let r;
-    //
-    // {
-    //     let x = 5;
-    //
-    //     // r is a reference to x
-    //     r = &x; // error here because x is dropped after this block
-    // }
-    //
-    // println!("r: {}", r);
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 // test
