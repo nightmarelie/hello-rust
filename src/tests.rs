c91be959bc57c2a32bbdcf4cfc89f4bdca89bea4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-05 10:20:16 -0600

    feat(tests): return result in test case

--- src/tests.rs
+++ src/tests.rs
@@ -71,4 +71,13 @@ mod tests {
     fn test_run() {
         panic!("test")
     }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 }
