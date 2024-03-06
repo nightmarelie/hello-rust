f9dc60190fd53eb9225860d5b0e524c6c483c4c2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-05 20:50:41 -0600

    chore: clean up

--- src/life_time.rs
+++ src/life_time.rs
@@ -63,7 +63,7 @@ pub fn example() {
 // &i32       // a reference
 // &'a i32    // a reference with an explicit lifetime
 // &'a mut i32// a mutable reference with an explicit lifetime
-fn longest <'a> (x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
