dadd8ac82a1c6b906af0708b3fcbc8d4aad28cb3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-08 21:39:30 -0600

    feat(closure): simulate some work

--- src/closures.rs
+++ src/closures.rs
@@ -8,3 +8,27 @@ pub fn simulated_expensive_calculation(intensity: u32) -> u32 {
     thread::sleep(Duration::from_secs(2));
     intensity
 }
+
+pub fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result = simulated_expensive_calculation(intensity);
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result
+            );
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod closures;
 
 fn main() {
-    closures::simulated_expensive_calculation(2);
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    closures::generate_workout(simulated_user_specified_value, simulated_random_number);
 }
