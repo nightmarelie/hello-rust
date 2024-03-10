3afa49435ea0cbd59e7199dd5a34e05157ed0ab7
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-09 21:56:29 -0600

    doc(closure): add more info

--- src/closures.rs
+++ src/closures.rs
@@ -76,4 +76,8 @@ pub fn capturing () {
 
     assert!(equal_to_x(y));
     println!("The result of equal_to_x is: {}", equal_to_x(y));
+
+    // FnOnce consumes the variables it captures from its enclosing scope, known as the closure’s environment. To consume the captured variables, the closure must take ownership of these variables and move them into the closure when it is defined. The Once part of the name represents the fact that the closure can’t take ownership of the same variables more than once, so it can be called only once.
+    // FnMut can change the environment because it mutably borrows values.
+    // Fn borrows values from the environment immutably.
 }
