0949bf033a6fa7e7904268a5553b6e96e73cfa57
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-10 20:01:11 -0600

    feat(iterators): init iterators

---
+++ src/iterators.rs
@@ -0,0 +1,13 @@
+pub fn example() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        example();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-mod closures;
+mod iterators;
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
-
-    // closures::generate_workout(simulated_user_specified_value, simulated_random_number);
-    closures::capturing();
+    iterators::example();
 }
