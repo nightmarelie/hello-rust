ee05458fcc65faa28f70a697588936753727f7b8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-10 20:01:11 -0600

    feat(iterators): add collect consumer

--- src/iterators.rs
+++ src/iterators.rs
@@ -57,5 +57,27 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(3));
         assert_eq!(v1_iter.next(), None);
     }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        // sum is a consuming adaptor
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn iterator_map() {
+        let v1 = vec![1, 2, 3];
+
+        // map is a lazy adaptor. Collect is a consuming adaptor
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+        assert_eq!(v2, vec![2, 3, 4]);
+    }
 }
 
