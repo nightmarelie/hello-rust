7411833d11528eea7446b99f9d2240dc4b446067
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-10 20:01:11 -0600

    feat(iterators): iterate throw vector

--- src/iterators.rs
+++ src/iterators.rs
@@ -1,5 +1,11 @@
 pub fn example() {
+    let vec = vec![1, 2, 3];
+    // iterator is lazy, it does not do anything until it is used
+    let vec_iter = vec.iter();
 
+    for val in vec_iter {
+        println!("Got: {}", val);
+    }
 }
 
 #[cfg(test)]
