190012dc46e7526894a551f8eb791c8f5fb2da12
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-10 20:01:11 -0600

    feat(iterators): more examples

--- src/iterators.rs
+++ src/iterators.rs
@@ -36,6 +36,26 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(&2));
         assert_eq!(v1_iter.next(), Some(&3));
         assert_eq!(v1_iter.next(), None);
+
+
+        let mut v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter_mut();
+
+        assert_eq!(v1_iter.next(), Some(&mut 1));
+        assert_eq!(v1_iter.next(), Some(&mut 2));
+        assert_eq!(v1_iter.next(), Some(&mut 3));
+        assert_eq!(v1_iter.next(), None);
+
+
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.into_iter();
+
+        assert_eq!(v1_iter.next(), Some(1));
+        assert_eq!(v1_iter.next(), Some(2));
+        assert_eq!(v1_iter.next(), Some(3));
+        assert_eq!(v1_iter.next(), None);
     }
 }
 
