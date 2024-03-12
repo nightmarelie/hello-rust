a97a7366d9e10b7f198bc4b1f9135611cd870ae8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-11 22:01:05 -0600

    feat(iterators): show custom functions

--- src/iterators.rs
+++ src/iterators.rs
@@ -155,8 +155,16 @@ mod tests {
         assert_eq!(counter.next(), Some(4));
         assert_eq!(counter.next(), Some(5));
         assert_eq!(counter.next(), None);
+    }
 
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
 
+        assert_eq!(18, sum);
     }
 }
 
