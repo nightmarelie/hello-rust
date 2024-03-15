9733d107e91cf7d7e055fa2f652dc13913ea518b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-15 10:01:11 -0600

    feat(smart-pointers): list struct

--- src/smart_pointer.rs
+++ src/smart_pointer.rs
@@ -1,5 +1,17 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+
 pub fn example () {
     let b = Box::new(5);
 
     println!("b = {}", b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("list = {:?}", list);
 }
