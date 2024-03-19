b05fa432c11ca3f5b444a68488f8ca491eb76329
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-18 20:43:13 -0600

    doc(deref_trait): add comments

--- src/deref_trait.rs
+++ src/deref_trait.rs
@@ -3,10 +3,10 @@ pub fn example() {
     let y = &x;
 
     assert_eq!(5, x);
-    assert_eq!(5, *y); // that mean dereference y
+    assert_eq!(5, *y); // that mean dereference y. It will follow the reference to the value it’s pointing to.
 
     let x = 5;
-    let y = Box::new(x);
+    let y = Box::new(x); // Box<T> is a smart pointer because it implements the Deref trait, which allows Box<T> values to be treated like references.
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
