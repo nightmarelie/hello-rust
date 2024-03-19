5d139d76a4ec596d1f073a56379412c3bd364218
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-18 20:59:31 -0600

    feat(drop_trait): impl drop trait

--- src/deref_trait.rs
+++ src/deref_trait.rs
@@ -45,6 +45,16 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
