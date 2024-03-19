fe97846386b2461108f18b17f132bf8fd1344524
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-18 20:49:35 -0600

    feat(deref_trait): add custom box

--- src/deref_trait.rs
+++ src/deref_trait.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 pub fn example() {
     let x = 5;
     let y = &x;
@@ -11,11 +13,27 @@ pub fn example() {
     assert_eq!(5, x);
     assert_eq!(5, *y);
 
-    // let x = 5;
-    // let y = MyBox::new(x);
-    //
-    // assert_eq!(5, x);
-    // assert_eq!(5, *y);
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
 
 #[cfg(test)]
