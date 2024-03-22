e07045a3a9b87c3b964845733edfa6f0a3218ab1
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-21 21:05:56 -0600

    feat(smart_pointer): change impl

--- src/smart_pointer.rs
+++ src/smart_pointer.rs
@@ -56,13 +56,19 @@ pub fn example () {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&Rc::clone(&branch));
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
 
 pub trait Messenger {
