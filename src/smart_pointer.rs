53dfe6b82aace31e9b3919a10a8811c960aa0028
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-21 20:53:07 -0600

    feat(smart_pointer): add tail fn

--- src/smart_pointer.rs
+++ src/smart_pointer.rs
@@ -1,11 +1,21 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 use List::{Cons, Nil};
 
 #[derive(Debug)]
 enum List {
     // reference counting
-    Cons(i32, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
 }
 
 // space is allocated on the heap for the values
