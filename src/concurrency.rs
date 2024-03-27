e88c9830bf49c593d3d104239e26591648c7d3e8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-26 21:05:03 -0600

    feat(concurrency): swap order of execution

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -9,15 +9,13 @@ pub fn example() {
         }
     });
 
+    handler.join().unwrap(); // wait for the spawned thread to finish executing
+
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
 
-
-
     // at this point the spawned thread may or may not have finished executing
     // if the spawned thread has not finished executing, the main thread will exit and the spawned thread will be terminated
-
-    handler.join().unwrap(); // wait for the spawned thread to finish executing
 }
