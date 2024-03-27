e927c9abc8741bcda9fe943a3b2525ecc4098d31
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-26 21:03:47 -0600

    feat(concurrency): wait for spawned treads

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 pub fn example() {
-    thread::spawn(|| {
+    let handler = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -14,6 +14,10 @@ pub fn example() {
         thread::sleep(Duration::from_millis(1));
     }
 
+
+
     // at this point the spawned thread may or may not have finished executing
     // if the spawned thread has not finished executing, the main thread will exit and the spawned thread will be terminated
+
+    handler.join().unwrap(); // wait for the spawned thread to finish executing
 }
