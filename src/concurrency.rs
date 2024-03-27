60c50b3bb2673d07e280cd141f20b00b1f855f6b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-26 21:02:18 -0600

    doc(concurrency): add docs

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -13,4 +13,7 @@ pub fn example() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    // at this point the spawned thread may or may not have finished executing
+    // if the spawned thread has not finished executing, the main thread will exit and the spawned thread will be terminated
 }
