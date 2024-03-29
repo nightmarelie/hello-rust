25bd58ce93a340247412a2dd125aad3d74328ce6
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-28 21:41:12 -0600

    feat(concurrency): try to fix error

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc; // multiple producer, single consumer
@@ -81,11 +82,12 @@ pub fn example3 () {
     println!("m = {:?}", m);
 
 
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
 
     let mut handles = vec![];
 
     for _ in 0..10 {
+        let counter = Rc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
