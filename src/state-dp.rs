7004c9b373e066b0b1493b4de5d3706d75eff9b7
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-30 20:44:51 -0600

    doc(state_dp): add post struct

--- src/state-dp.rs
+++ src/state-dp.rs
@@ -1,3 +1,36 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
 
 pub fn example () {
     let mut post = Post::new();
