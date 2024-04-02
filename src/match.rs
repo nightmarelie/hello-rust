89579c57d46d330d59f616f56d3bf2313381ce8d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-01 22:52:35 -0600

    feat(matching): while example

--- src/match.rs
+++ src/match.rs
@@ -15,4 +15,14 @@ pub fn example () {
         Language::JavaScript => println!("JavaScript is fun!"),
         // _ => println!("Some other language!"), // This is not needed because we are matching all the variants. The _ pattern will match all values (catchall pattern).
     }
+
+    let mut stack = vec![];
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
 }
