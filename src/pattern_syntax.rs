0f2304db896e79b236b465c77e92576c34e208b8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-02 21:59:47 -0600

    feat(pattern-syntax): second syntax

--- src/pattern_syntax.rs
+++ src/pattern_syntax.rs
@@ -1,10 +1,19 @@
 pub fn example() {
     let x = 1;
-    
+
     match x {
         1 => println!("one"),
         2 => println!("two"),
         3 => println!("three"),
         _ => println!("anything"),
+    }
+
+    let x = Some(5);
+    let y = 5;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y), // y is shadowed
+        _ => println!("Default case, x = {:?}", x),
     }
 }
