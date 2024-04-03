835282446dcfa5686c0ea1541b82b4c865e45566
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-02 22:01:21 -0600

    feat(pattern-syntax): third syntax

--- src/pattern_syntax.rs
+++ src/pattern_syntax.rs
@@ -9,11 +9,28 @@ pub fn example() {
     }
 
     let x = Some(5);
-    let y = 5;
+    let _y = 5;
 
     match x {
         Some(50) => println!("Got 50"),
         Some(y) => println!("Matched, y = {:?}", y), // y is shadowed
         _ => println!("Default case, x = {:?}", x),
+    }
+
+    // Multiple patterns
+    let x = 1;
+    
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+    
+    // Range patterns
+    let x = 5;
+    
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something"),
     }
 }
