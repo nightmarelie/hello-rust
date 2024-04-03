3cc209844718387fcb86cda910637a40095c13af
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-03 10:01:11 -0600

    feat(unsafe_code): dereferencing a raw pointers

--- src/unsafe_code.rs
+++ src/unsafe_code.rs
@@ -3,6 +3,11 @@ pub fn example() {
 
     // 1. Dereferencing a raw pointer
     let mut num = 5;
+    let r1 = &num as *const i32; // immutable raw pointer
+    let r2 = &mut num as *mut i32; // mutable raw pointer
+    
+    let address = 0x012345usize;
+    let r = address as *const i32;
 
     // 2. Call an unsafe function or method
 
