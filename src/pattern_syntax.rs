e4e4cb3de94cf3167c811e1fa0f83d08592ec6d3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-02 22:19:04 -0600

    feat(pattern-syntax): another examples

--- src/pattern_syntax.rs
+++ src/pattern_syntax.rs
@@ -23,6 +23,10 @@ enum Message {
     ChangeColor2(Color),
 }
 
+enum Message2 {
+    Hello { id: i32 },
+}
+
 pub fn example() {
     // Patterns
 
@@ -111,13 +115,79 @@ pub fn example() {
             )
         }
     }
-    
+
     // Ignoring values in a pattern
     let _numbers = (2, 4, 8, 16, 32);
-    
+
     match _numbers {
         (first, _, third, _, fifth) => {
             println!("Some numbers: {}, {}, {}", first, third, fifth)
         }
     }
+
+    let mut _settings_value = Some(5);
+    let new_settings_value = Some(10);
+
+    match (_settings_value, new_settings_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            _settings_value = new_settings_value;
+        }
+    }
+
+    println!("new settings value is {:?}", _settings_value);
+
+    // using range syntax for ignoring remaining parts of a value
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        }
+    }
+
+    // match guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+    
+    // shadowing in match guards
+    let x = Some(5);
+    
+    match x {
+        Some(5) => println!("five"),
+        Some(_) => println!("some other number"),
+        None => (),
+    }
+    
+    // assigning a value to a variable in a match guard
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    // assigning a value using @ at operator
+    
+    let msg = Message2::Hello { id: 5 };
+    
+    match msg {
+        Message2::Hello { id: id_variable @ 3..=7 } => {
+            println!("Found an id in range: {}", id_variable)
+        }
+        Message2::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message2::Hello { id } => {
+            println!("Found some other id: {}", id)
+        }
+    }   
 }
