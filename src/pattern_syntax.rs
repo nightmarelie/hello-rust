ff59dd5f32c838a7c8b9c3c899f0d04010315f7b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-02 22:05:51 -0600

    feat(pattern-syntax): destructure enums

--- src/pattern_syntax.rs
+++ src/pattern_syntax.rs
@@ -3,6 +3,13 @@ struct Point {
     y: i32,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 pub fn example() {
     let x = 1;
 
@@ -52,4 +59,26 @@ pub fn example() {
     
     assert_eq!(0, a);
     assert_eq!(7, b);
+    
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+    
+    // destructing enums
+    let msg = Message::ChangeColor(0, 160, 255);
+    
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+    }
 }
