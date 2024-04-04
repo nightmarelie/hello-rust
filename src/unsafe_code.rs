3435058386e1159043bf9da25d0d845b728fb919
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-03 20:43:19 -0600

    feat(unsafe_code): template with rules

---
+++ src/unsafe_code.rs
@@ -0,0 +1,14 @@
+pub fn example() {
+    // Unsafe code ability:
+
+    // 1. Dereferencing a raw pointer
+    let mut num = 5;
+
+    // 2. Call an unsafe function or method
+
+    // 3. Access or modify a mutable static variable
+
+    // 4. Implement an unsafe trait
+
+    // 5. Access fields of unions
+}
