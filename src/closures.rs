c0ea8660bc3662a7306eeea7775bf9a7032c314d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-07-30 10:01:15 -0600

    feat(closures): comment exmpl

--- src/closures.rs
+++ src/closures.rs
@@ -85,12 +85,12 @@ pub fn capturing() {
 #[allow(dead_code)]
 pub fn example() {
     // similarity between fn and closure  
-    fn add_one_v1(x: u32) -> u32 {
-        x + 1
-    }
-    let add_one_v2 = |x: u32| -> u32 { x + 1 };
-    let add_one_v3 = |x| x + 1;
-    let add_one_v4 = |x| x + 1;
+    // fn add_one_v1(x: u32) -> u32 {
+    //     x + 1
+    // }
+    // let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    // let add_one_v3 = |x| x + 1;
+    // let add_one_v4 = |x| x + 1;
 
     let list_of_numbers = vec![1, 2, 3];
 
