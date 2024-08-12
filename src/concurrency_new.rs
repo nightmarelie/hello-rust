71b6c3851e9f0a422feaca816ebcf0197e715da5
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-08-12 10:01:15 -0600

    feat(cuncurrency): whait until all thread executed

--- src/concurrency_new.rs
+++ src/concurrency_new.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 pub fn example() -> () {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1));    
@@ -13,4 +13,6 @@ pub fn example() -> () {
         println!("hi number {i} from main thread!");
         thread::sleep(Duration::from_millis(1));
     } 
+    
+    handle.join().unwrap();
 }
