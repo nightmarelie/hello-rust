8795170de559bf5063a5377f8a80bab4ff7623b9
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-08-14 10:01:15 -0600

    feat(cuncurrency): try to use variable from outside scope

--- src/concurrency_new.rs
+++ src/concurrency_new.rs
@@ -5,14 +5,22 @@ pub fn example() -> () {
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));    
+            thread::sleep(Duration::from_millis(1));
         }
     });
-    
+
     handle.join().unwrap();
-    
+
     for i in 1..5 {
         println!("hi number {i} from main thread!");
         thread::sleep(Duration::from_millis(1));
-    } 
+    }
+
+    let v = vec![1, 2, 3];
+    
+    let handle = thread::spawn(|| {
+        println!("here's a vector: {v:?}");
+    });
+    
+    handle.join().unwrap();
 }
