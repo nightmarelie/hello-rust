a7abd9025600c2672646ab410f759b986950474a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-08-23 10:01:15 -0600

    feat(oop): create an example fn

--- src/design_pattern.rs
+++ src/design_pattern.rs
@@ -7,3 +7,13 @@ trait State {}
 struct Draft {}
 
 impl State for Draft {}
+
+impl Post {
+    fn new() -> Post {
+        Post { state: None }
+    }
+}
+
+pub fn example() {
+    let mut post = Post::new();
+}
