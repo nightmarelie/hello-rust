11087fb240762cd58e29b0f8fe365c8d43a13a0a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-08-31 10:01:15 -0600

    feat(oop): approve review

--- src/design_pattern.rs
+++ src/design_pattern.rs
@@ -44,6 +44,12 @@ impl Post {
             self.state = Some(s.request_review())
         }
     }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
 }
 
 pub fn example() {
