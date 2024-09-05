2ae02c0793d8f7113e8e93a2738446938090e68d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-05 10:01:15 -0600

    feat(oop): modify an example

--- src/design_pattern.rs
+++ src/design_pattern.rs
@@ -84,6 +84,11 @@ pub fn example() {
     let mut post = Post::new();
     
     post.add_text("I ate a salad for lunch today");
-    
-    assert_eq!("I ate a salad for lunch today", post.content())
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+
 }
