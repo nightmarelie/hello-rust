eec27b49178fb96bee9e27c3303eee5eeb89b5cc
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-10 15:32:42 -0600

    feat(closures): show type annotations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod closures;
 
-use crate::closures::{Inventory, ShirtColor};
+use std::thread;
+use std::time::Duration;
+use closures::{Inventory, ShirtColor};
 
 fn main() {
     let store = Inventory {
@@ -16,4 +18,13 @@ fn main() {
     let giveaway2 = store.giveaway(user_pref2);
 
     println!("The user with preference {:?} gets {:?}", user_pref2, giveaway2);
+    
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    
+    println!("{}", expensive_closure(13));
 }
