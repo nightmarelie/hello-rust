5c5e913c87213d4d7bda8068a589c021d88d53a1
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-17 17:29:06 -0600

    feat(concurrency): prepare

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,5 @@
-mod closures;
-
-use std::thread;
-use std::time::Duration;
-use closures::{Inventory, ShirtColor};
+mod concurrency_new;
 
 fn main() {
-    let store = Inventory {
-        shirts: vec![ShirtColor::Blue, ShirtColor::Blue, ShirtColor::Red],
-    };
-
-    let user_pref1 = Some(ShirtColor::Red);
-    let giveaway1 = store.giveaway(user_pref1);
-
-    println!("The user with preference {:?} gets {:?}", user_pref1, giveaway1);
     
-    let user_pref2 = None;
-    let giveaway2 = store.giveaway(user_pref2);
-
-    println!("The user with preference {:?} gets {:?}", user_pref2, giveaway2);
-    
-    let expensive_closure = |num: u32| -> u32 {
-        println!("calculating slowly...");
-        
-        thread::sleep(Duration::from_secs(2));
-        num
-    };
-    
-    println!("{}", expensive_closure(13));
 }
