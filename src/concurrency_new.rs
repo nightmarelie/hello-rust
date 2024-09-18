f1a9e66d52d86497358970e7af91fd174d4b0ac3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-18 10:01:15 -0600

    feat(cuncurrency): send multiple values

--- src/concurrency_new.rs
+++ src/concurrency_new.rs
@@ -6,13 +6,16 @@ pub fn example() -> () {
     // introduce channel
     // tx stands for transmitter
     // rx stands for receiver
-    let (tx, _rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
     
     thread::spawn(move || {
         let val = String::from("hi");
         
         tx.send(val).unwrap();
     });
+    
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
     
     let handle = thread::spawn(|| {
         for i in 1..10 {
@@ -35,4 +38,24 @@ pub fn example() -> () {
     });
     
     handle.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
