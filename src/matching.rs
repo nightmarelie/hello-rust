9b364278757bd6aaca387a7b7f36f6e297671187
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-23 10:01:15 -0600

    feat(matching): update rust

--- src/matching.rs
+++ src/matching.rs
@@ -109,7 +109,7 @@ pub fn example() {
         _ => println!("Default case, x = {x:?}"),
     }
 
-    println!("at the end: x = {x:?}, y = {y}");
+    // println!("at the end: x = {x:?}, y = {y}");
 
     // matching ranges of values with ..=
     let x = 5;
@@ -225,21 +225,21 @@ pub fn example() {
     }
 
     // bindings
-    enum Message {
-        Hello { id: i32 },
-    }
-
-    let msg = Message::Hello { id: 5 };
-
-    match msg {
-        Message::Hello {
-            id: id_variable @ 3..=7,
-        } => println!("Found an id in range: {id_variable}"),
-        Message::Hello { id: 10..=12 } => {
-            println!("Found an id in another range")
-        }
-        Message::Hello { id } => println!("Found some other id: {id}"),
-    }
+    // enum Message {
+    //     Hello { id: i32 },
+    // }
+    // 
+    // let msg = Message::Hello { id: 5 };
+    // 
+    // match msg {
+    //     Message::Hello {
+    //         id: id_variable @ 3..=7,
+    //     } => println!("Found an id in range: {id_variable}"),
+    //     Message::Hello { id: 10..=12 } => {
+    //         println!("Found an id in another range")
+    //     }
+    //     Message::Hello { id } => println!("Found some other id: {id}"),
+    // }
 }
 
 // ignoring entire value
