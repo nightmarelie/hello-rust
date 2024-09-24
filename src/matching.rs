b141b6491cd13ba80bb396378a5f276e23c1bfc6
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-24 10:01:15 -0600

    feat(matching): extra conditionals with match guards

--- src/matching.rs
+++ src/matching.rs
@@ -214,6 +214,15 @@ pub fn example() {
     match origin {
         Point1 { x, .. } => println!("x is {x}"),
     }
+
+    // extra conditionals with match guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
 }
 
 // ignoring entire value
