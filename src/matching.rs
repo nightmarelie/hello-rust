24c7103de693f57ce3dd3607b4e8d638f189c34f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-24 10:01:15 -0600

    feat(matching): destructuring structs

--- src/matching.rs
+++ src/matching.rs
@@ -108,6 +108,20 @@ pub fn example() {
         'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else"),
     }
+
+    // destructuring structs
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    fn main() {
+        let p = Point { x: 0, y: 7 };
+
+        let Point { x: a, y: b } = p;
+        assert_eq!(0, a);
+        assert_eq!(7, b);
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
