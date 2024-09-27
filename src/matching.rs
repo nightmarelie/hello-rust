a4d282328faf1354e72ef14e6fc697092d7608e2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-27 10:01:15 -0600

    feat(matching): enum matching

--- src/matching.rs
+++ src/matching.rs
@@ -5,6 +5,13 @@ enum Language {
     JavaScript,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 pub fn example() {
     let v = vec!['a', 'b', 'c'];
 
@@ -139,6 +146,25 @@ pub fn example() {
             println!("On neither axis: ({x}, {y})");
         }
     }
+
+    // enum matching
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}")
+        }
+    }
+
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
