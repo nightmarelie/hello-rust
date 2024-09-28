32e066dde3595497325117e3a4850a5488247c44
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-09-28 10:01:15 -0600

    feat(matching): nested structs

--- src/matching.rs
+++ src/matching.rs
@@ -5,11 +5,23 @@ enum Language {
     JavaScript,
 }
 
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 }
 
 pub fn example() {
@@ -148,23 +160,35 @@ pub fn example() {
     }
 
     // enum matching
-    let msg = Message::ChangeColor(0, 160, 255);
+    // let msg = Message::ChangeColor(0, 160, 255);
+    // 
+    // match msg {
+    //     Message::Quit => {
+    //         println!("The Quit variant has no data to destructure.");
+    //     }
+    //     Message::Move { x, y } => {
+    //         println!("Move in the x direction {x} and in the y direction {y}");
+    //     }
+    //     Message::Write(text) => {
+    //         println!("Text message: {text}");
+    //     }
+    //     Message::ChangeColor(r, g, b) => {
+    //         println!("Change the color to red {r}, green {g}, and blue {b}")
+    //     }
+    // }
+
+    // nested structs
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.");
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
         }
-        Message::Move { x, y } => {
-            println!("Move in the x direction {x} and in the y direction {y}");
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {s}, value {v}")
         }
-        Message::Write(text) => {
-            println!("Text message: {text}");
-        }
-        Message::ChangeColor(r, g, b) => {
-            println!("Change the color to red {r}, green {g}, and blue {b}")
-        }
+        _ => (),
     }
-
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
