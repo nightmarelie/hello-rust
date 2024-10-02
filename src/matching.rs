b2d822ffdf18dbe1e123a8ba7a7649ccc2ee068e
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-10-01 19:37:11 -0600

    feat(matching): Example of if let else if let

--- src/matching.rs
+++ src/matching.rs
@@ -69,3 +69,23 @@ pub fn example() {
 fn print_coordinates(&(x, y): &(i32, i32)) {
     println!("Current location: ({}, {})", x, y);
 }
+
+fn example_of_if_let_else_if_let() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
