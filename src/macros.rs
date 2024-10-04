6a9fdf9263aaa91d8f109a98ad81c269521823d3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-10-03 20:36:45 -0600

    feat(macro): create hello macro

--- src/macros.rs
+++ src/macros.rs
@@ -34,3 +34,17 @@ macro_rules! vec {
         }
     };
 }
+
+pub trait HelloMacro {
+    fn hello_macro();
+}
+
+use hello_macro::HelloMacro;
+
+struct Pancakes;
+
+impl HelloMacro for Pancakes {
+    fn hello_macro() {
+        println!("Hello, Macro! My name is Pancakes!");
+    }
+}
